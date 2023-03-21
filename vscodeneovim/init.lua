1ab9e0160dd21f77194ae787d88b67a021e97870
frazrepo <frazvisuals@gmail.com>
2023-03-21 10:20:33 +0100

    Update init.lua

--- vscodeneovim/init.lua
+++ vscodeneovim/init.lua
@@ -207,7 +207,20 @@ if vim.g.vscode then
         -- Buffer Helpers
         -----------------------------------------------------------
         -- tim pope plugins
-        'tpope/vim-surround',
+        -- 'tpope/vim-surround',
+               
+        -- Replace with nvim surround
+        {
+            "kylechui/nvim-surround",
+            version = "*", -- Use for stability; omit to use `main` branch for the latest features
+            event = "VeryLazy",
+            config = function()
+                require("nvim-surround").setup({
+                    -- Configuration here, or leave empty to use defaults
+                })
+            end
+        },
+               
 
         --  repeat surround action
         'tpope/vim-repeat',
