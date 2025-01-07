31f9389f7b4591273960b7b284cb150797a13a97
frazrepo <frazvisuals@gmail.com>
2025-01-07 20:27:07 +0100

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -174,6 +174,64 @@ function tab_title(tab_info)
   return tab_info.active_pane.title
 end
 
+icons = {
+  ["C:\\Windows\\system32\\cmd.exe"] = wezterm.nerdfonts.md_console_line,
+  ["Topgrade"] = wezterm.nerdfonts.md_rocket_launch,
+  ["bash"] = wezterm.nerdfonts.cod_terminal_bash,
+  ["btm"] = wezterm.nerdfonts.mdi_chart_donut_variant,
+  ["cargo"] = wezterm.nerdfonts.dev_rust,
+  ["curl"] = wezterm.nerdfonts.mdi_flattr,
+  ["docker"] = wezterm.nerdfonts.linux_docker,
+  ["docker-compose"] = wezterm.nerdfonts.linux_docker,
+  ["fish"] = wezterm.nerdfonts.md_fish,
+  ["gh"] = wezterm.nerdfonts.dev_github_badge,
+  ["git"] = wezterm.nerdfonts.dev_git,
+  ["go"] = wezterm.nerdfonts.seti_go,
+  ["htop"] = wezterm.nerdfonts.md_chart_areaspline,
+  ["btop"] = wezterm.nerdfonts.md_chart_areaspline,
+  ["kubectl"] = wezterm.nerdfonts.linux_docker,
+  ["kuberlr"] = wezterm.nerdfonts.linux_docker,
+  ["lazydocker"] = wezterm.nerdfonts.linux_docker,
+  ["lua"] = wezterm.nerdfonts.seti_lua,
+  ["make"] = wezterm.nerdfonts.seti_makefile,
+  ["node"] = wezterm.nerdfonts.mdi_hexagon,
+  ["nvim"] = wezterm.nerdfonts.custom_vim,
+  ["pacman"] = "󰮯 ",
+  ["paru"] = "󰮯 ",
+  ["psql"] = wezterm.nerdfonts.dev_postgresql,
+  ["pwsh.exe"] = wezterm.nerdfonts.md_console,
+  ["powershell.exe"] = wezterm.nerdfonts.cod_terminal_powershell,
+  ["wslhost.exe"] = wezterm.nerdfonts.cod_terminal_linux,
+  ["ruby"] = wezterm.nerdfonts.cod_ruby,
+  ["sudo"] = wezterm.nerdfonts.fa_hashtag,
+  ["vim"] = wezterm.nerdfonts.dev_vim,
+  ["wget"] = wezterm.nerdfonts.mdi_arrow_down_box,
+  ["zsh"] = wezterm.nerdfonts.dev_terminal,
+  ["lazygit"] = wezterm.nerdfonts.cod_github,
+}
+function tab_title2(tab, max_width)
+  local title = (tab.tab_title and #tab.tab_title > 0) and tab.tab_title or tab.active_pane.title
+  local process, other = title:match("^(%S+)%s*%-?%s*%s*(.*)$")
+
+  if icons[process] then
+    title = icons[process] .. " " .. (other or "")
+  end
+
+  local is_zoomed = false
+  for _, pane in ipairs(tab.panes) do
+    if pane.is_zoomed then
+      is_zoomed = true
+      break
+    end
+  end
+  if is_zoomed then -- or (#tab.panes > 1 and not tab.is_active) then
+    title = " " .. title
+  end
+
+  title = wezterm.truncate_right(title, max_width - 3)
+  return " " .. title .. " "
+end
+
 wezterm.on(
   'format-tab-title',
   function(tab, tabs, panes, config, hover, max_width)
@@ -182,7 +240,6 @@ wezterm.on(
     local foreground = '#808080'
 
     if tab.is_active then
-      -- background = '#2b2042'
       background = '#7c59c9'
       foreground = '#c0c0c0'
     elseif hover then
@@ -192,7 +249,8 @@ wezterm.on(
 
     local edge_foreground = background
 
-    local title = tab_title(tab)
+    -- local title = tab_title(tab)
+    local title = tab_title2(tab, max_width)
 
     -- ensure that the titles fit in the available space,
     -- and that we have room for the edges.
@@ -406,10 +464,12 @@ wezterm.on("update-status", function(window, pane)
 	if cwd then
 		if type(cwd) == "userdata" then
 			-- Wezterm introduced the URL object in 20240127-113634-bbcac864
-			cwd = basename(cwd.file_path)
+			-- cwd = basename(cwd.file_path)
+			cwd = cwd.file_path
 		else
 			-- 20230712-072601-f4abf8fd or earlier version
-			cwd = basename(cwd)
+			-- cwd = basename(cwd)
+			-- cwd = basename(cwd)
 		end
 	else
 		cwd = ""
